d5a34cdd589875d9031373b500cbcb8c7946bc11
Alek <sforaaleksander@gmail.com>
2020-04-07 13:28:57 +0200

    filling up coords and player class

--- src/Coords.java
+++ src/Coords.java
@@ -1,5 +1,21 @@
 package src;
 
 public class Coords {
+    private int posY;
+    private int posX;
+
+    Coords(int posY, int posX){
+        this.posY = posY;
+        this.posX = posX;
+    }
+
+    public int getposY(){
+        return posY;
+    }
+
+    public int getposX(){
+        return posX;
+    }
+
 
 }
---
+++ src/Engine.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Engine {
+
+    public static void playerMove(){
+        
+    Coords W = new Coords(-1, 0);
+    Coords S = new Coords(1, 0);
+    Coords A = new Coords(0, -1);
+    Coords D = new Coords(0, 1);
+    }
+}
